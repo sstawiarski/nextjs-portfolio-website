a0a330852a742fb8e5914b278230f9d8398206bc
Shawn Stawiarski <contact@shawnstawiarski.com>
2021-05-04 17:14:13 -0400

    feat: add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,11 @@ class MyDocument extends Document {
                         rel="preload"
                         as="style"
                     />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                    <link rel="manifest" href="/site.webmanifest" />
+                    <meta name="theme-color" content="#1F2838" />
                     <meta
                         name="description"
                         content="Shawn Stawiarski is a software engineer based in Pittsburgh, PA"
