a7efc6f626d1c25e4a8ffe3367dd0f33dbefb6f1
Shawn Stawiarski <contact@shawnstawiarski.com>
2021-05-06 15:41:39 -0400

    refactor: improve code style

--- components/ProjectDetail/FooterItem.tsx
+++ components/ProjectDetail/FooterItem.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { FooterItemProps } from "../../@types";
 
-import JavaIcon from '../../public/icons/java.svg'
-import MysqlIcon from '../../public/icons/mysql.svg';
-import ReactIcon from '../../public/icons/react.svg';
-import MongoIcon from '../../public/icons/mongodb.svg';
-import NextjsIcon from '../../public/icons/nextjs.svg';
-import TypeScriptIcon from '../../public/icons/typescript.svg';
+import JavaIcon from "../../public/icons/java.svg";
+import MysqlIcon from "../../public/icons/mysql.svg";
+import ReactIcon from "../../public/icons/react.svg";
+import MongoIcon from "../../public/icons/mongodb.svg";
+import NextjsIcon from "../../public/icons/nextjs.svg";
+import TypeScriptIcon from "../../public/icons/typescript.svg";
 
 const FooterItem: React.FC<FooterItemProps> = ({ text = "", icon = "", additional = "" }) => {
     const Icon = getIcon(icon);
@@ -14,9 +14,16 @@ const FooterItem: React.FC<FooterItemProps> = ({ text = "", icon = "", additiona
         <div className="flex flex-col justify-between">
             <div className="flex flex-col sm:flex-row items-center  sm:justify-around mb-3">
                 <Icon className="h-7 w-24 fill-current" />
-                {(icon !== "MySQL" && icon !== "MongoDB" && icon !== "Next.js") && (<span className={["font-normal text-lg mt-1", icon === "Amazon Web Services" ? "" : "sm:-ml-10 lg:-ml-28"].join(" ")}>{text}</span>)}
+                {icon !== "MySQL" && icon !== "MongoDB" && icon !== "Next.js" && (
+                    <span
+                        className={[
+                            "font-normal text-lg mt-1",
+                            icon === "Amazon Web Services" ? "" : "sm:-ml-10 lg:-ml-28",
+                        ].join(" ")}>
+                        {text}
+                    </span>
+                )}
             </div>
-
 
             {additional.split("\\n").map((item, idx) => (
                 <React.Fragment key={idx}>
